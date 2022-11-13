0cf4b8c34e1f2964985392b8a8b7685ab2eafa38
robertsj <rschultzejena@gmail.com>
2022-11-13 08:58:26 +0100

    Install and configure ActiveModelSerializer
    It makes it easier to structure API responses according to the json-api
    format, documented on https://jsonapi.org/.
    
    It's worth being aware of the fact that the Gem is not in active
    development and alternatives are listed on the GitHub page, however,
    this approach seemed like the most straightforward.
    ActiveModelSerializer used to be part of the Rails library when
    installed as a API.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
+  routes.default_url_options[:host] = 'localhost:3000'
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
