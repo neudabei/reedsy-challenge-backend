527073ab3906ca85db9db6f10f349ac774d6b724
robertsj <rschultzejena@gmail.com>
2022-11-13 12:20:18 +0100

    Implement PATCH endpoint
    The endpoint allows updating the price of a ressource. It adheres to the
    standards laid out on
    
    https://jsonapi.org/

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def index
     render json: Product.all
   end
+
+  def update
+    product = Product.find(params[:id])
+
+    if product.update(product_params)
+      render json: product
+    else
+      render json: { errors: product.errors.full_messages }, status: :unprocessable_entity
+    end
+
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'Please make sure the product exists. You can use the GET /products endpoint for a list of all products.' },
+           status: :not_found
+  end
+
+  private
+
+  def product_params
+    params.require(:data).require(:attributes).permit(:code, :name, :price)
+  end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'ProductsController', :type => :request do
+  let(:product) { Product.create(code: 'mug', name: 'Reedsy Mug', price: 600) }
+
   describe '#index' do
     it 'returns the full list of products with attributes' do
       product = Product.create(code: 'mug', name: 'Reedsy Mug', price: 600)
@@ -33,6 +35,64 @@ RSpec.describe 'ProductsController', :type => :request do
 
         expect(response.body).to eq({ data: [] }.to_json)
         expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:request_body) {
+      { data:
+        {
+          id: product.id.to_s,
+          type: 'products',
+          attributes:
+          {
+            code: 'mug',
+            name: 'Reedsy Mug',
+            price: '800',
+          }
+        }
+      }
+    }
+
+    let(:response_body) {
+      {
+        "data": {
+          "id": product.id.to_s,
+          "type": "products",
+          "attributes": {
+            "code": "mug",
+            "name": "Reedsy Mug",
+            "price": "8.00",
+            "updated-at": product.reload.updated_at.strftime('%FT%T.%LZ')
+          }
+        }
+      }.to_json
+    }
+
+    before do
+      patch "/products/#{product.id}", params: request_body
+    end
+
+    it 'updates the product with the new price' do
+      expect(product.reload.price).to eq(800)
+    end
+
+    it 'responds with a 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'responds with the updated resource' do
+      expect(response.body).to eq(response_body)
+    end
+
+    context 'when no product with that id can be found' do
+      before do
+        patch "/products/#{Product.last.id + 1}", params: request_body
+      end
+
+      it 'responds with a 404' do
+        expect(response.status).to eq(404)
       end
     end
   end
